65314f8fd0e9e5e5068dedb4323b37b05a4724d0
webreflection <andrea.giammarchi@gmail.com>
2025-11-08 09:13:10 +0100

    fixed methods context too

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const proxies = new WeakMap;
-const handler = {};
+const cbs = new WeakMap;
 
-for (const key of Reflect.ownKeys(Reflect)) {
-  const method = Reflect[key];
-  handler[key] = key === 'get' || key === 'set' ?
-    // proxy as receiver is trouble with get / set
-    // @see https://es.discourse.group/t/reflect-set-args-receiver-throwing-for-no-reason/2462
-    ((target, ...rest) => method(target.deref(), ...rest.slice(0, -1))) :
-    ((target, ...rest) => method(target.deref(), ...rest))
-  ;
-}
+const { apply, get, set } = Reflect;
+
+const fn = { apply: (t, p, a) => apply(t, (p && deref(p)) ?? p, a) };
+
+const guard = cb => cbs.set(cb, new Proxy(cb, fn)).get(cb);
+
+// proxy as receiver is trouble with get / set
+// @see https://es.discourse.group/t/reflect-set-args-receiver-throwing-for-no-reason/2462
+const handler = {
+  get(target, prop) {
+    const r = get(target.deref(), prop);
+    return typeof r === 'function' ? (cbs.get(r) ?? guard(r)) : r;
+  },
+  set: (target, prop, value) => set(target.deref(), prop, value),
+};
+
+for (const key of Reflect.ownKeys(Reflect))
+  handler[key] ??= ((target, ...rest) => Reflect[key](target.deref(), ...rest));
 
 /**
  * @template T
